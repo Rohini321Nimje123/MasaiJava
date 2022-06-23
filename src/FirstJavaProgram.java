ef89108d5f273559629fbe81d7647b3f9cf99829
Rohini321Nimje123 <101380402+Rohini321Nimje123@users.noreply.github.com>
2022-06-23 19:30:40 +0530

    Initial commit

---
+++ src/FirstJavaProgram.java
@@ -0,0 +1,12 @@
+public class FirstJavaProgram {
+    public static void main(String[] args){
+        System.out.println("Name: Rohini Nimje");
+        System.out.println("Father’s Name: Lal Das Nimje");
+        System.out.println("Mother ‘s Name: Vinita Nimje");
+        System.out.println("Age: 28");
+        System.out.println("Gender: Female");
+        System.out.println("Address: Riddhi Siddhi Colony");
+        System.out.println("Mobile No.: 8305052079");
+    }
+
+}
